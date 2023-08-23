f4fe223ba81581c93ae538945bafda7dff36eb08
Dan Stroot <dan.stroot@gmail.com>
2023-08-23 08:17:47 -0700

    clean compile

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 86400,
+    loader: 'custom',
+    loaderFile: './lib/imgixLoader.ts',
+  },
+}
 
 module.exports = nextConfig
