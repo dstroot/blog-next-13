5b2742cb544f87c5debea257c770a6e6288cefdb
Dan Stroot <dan.stroot@gmail.com>
2023-08-24 18:46:18 -0700

    clean compile

---
+++ src/app/drafts/page.tsx
@@ -0,0 +1,33 @@
+import { allPosts } from 'contentlayer/generated'
+import { compareDesc } from 'date-fns'
+
+import { HeroPost } from '@/components/HeroPost'
+import { Intro } from '@/components/Intro'
+import { MoreStories } from '@/components/MoreStories'
+
+export default function Home() {
+  let posts = allPosts.sort((a, b) =>
+    compareDesc(new Date(a.date), new Date(b.date)),
+  )
+  // Remove any published posts
+  posts = posts.filter((posts) => !posts.published)
+
+  const heroPost = posts[0]
+  const morePosts = posts.slice(1)
+
+  return (
+    <div className="">
+      <Intro />
+      <HeroPost
+        title={heroPost.title}
+        coverImage={heroPost.coverImage}
+        date={heroPost.date}
+        author={heroPost.author}
+        slug={heroPost.slugAsParams}
+        excerpt={heroPost.excerpt}
+        stats={heroPost.stats}
+      />
+      <MoreStories posts={morePosts} />
+    </div>
+  )
+}
