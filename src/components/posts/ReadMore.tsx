10a079d68274627532c2db563bc95f53db4dc39f
Dan Stroot <dan.stroot@gmail.com>
2023-08-30 17:58:01 -0700

    updated dependencies

--- src/components/posts/ReadMore.tsx
+++ src/components/posts/ReadMore.tsx
@@ -5,10 +5,10 @@ import { Icons } from '@/components/Icons'
 export const ReadMore = ({ slug }: { slug: string }) => {
   return (
     <Link href={`/posts/${slug}`}>
-      <div className="group flex items-center space-x-2 ease-in-out">
-        <div className="text-lg font-semibold text-gray-900 transition-colors group-hover:underline dark:text-gray-100 md:text-xl">
+      <div className="group flex items-center ease-in-out">
+        <span className="text-lg font-semibold group-hover:underline md:text-xl">
           Read Post
-        </div>
+        </span>
         <Icons.chevronsRight className="h-6 w-6 md:h-8 md:w-8" />
       </div>
     </Link>
