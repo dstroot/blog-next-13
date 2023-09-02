11b25ce2c05c568529e46ef5a685f46922c1ce5c
Dan Stroot <dan.stroot@gmail.com>
2023-09-02 14:34:33 -0700

    updated dependencies

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,25 +1,25 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-import { env } from '@/config/env.mjs'
+// import { env } from '@/config/env.mjs'
 
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    {
-      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
-      missing: [
-        { type: 'header', key: 'next-router-prefetch' },
-        { type: 'header', key: 'purpose', value: 'prefetch' },
-      ],
-    },
-  ],
-}
+// export const config = {
+//   matcher: [
+//     /*
+//      * Match all request paths except for the ones starting with:
+//      * - api (API routes)
+//      * - _next/static (static files)
+//      * - _next/image (image optimization files)
+//      * - favicon.ico (favicon file)
+//      */
+//     {
+//       source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+//       missing: [
+//         { type: 'header', key: 'next-router-prefetch' },
+//         { type: 'header', key: 'purpose', value: 'prefetch' },
+//       ],
+//     },
+//   ],
+// }
 
 export function middleware(request: NextRequest) {
   const nonce = crypto.randomUUID()
