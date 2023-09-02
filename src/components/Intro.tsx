d113eefd593995651e9ca89718ff84ac9fbf4a99
Dan Stroot <dan.stroot@gmail.com>
2023-09-02 10:39:45 -0700

    updated dependencies

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -3,14 +3,12 @@ import { siteConfig } from '@/config/site'
 export const Intro = () => {
   return (
     <section className="mb-8 md:mb-12">
-      <div className="flex flex-col">
-        <div className="flex flex-col items-center md:flex-row md:justify-between">
-          <div className="text-4xl font-bold leading-tight tracking-tighter md:text-6xl lg:text-7xl">
-            {siteConfig.meta.name}
-          </div>
-          <div className="mt-2 text-2xl font-semibold italic md:text-2xl lg:text-3xl">
-            {siteConfig.meta.tagline}
-          </div>
+      <div className="flex flex-col items-center md:flex-row md:justify-between">
+        <h1 className="text-4xl font-bold leading-tight tracking-tighter md:text-6xl lg:text-7xl">
+          {siteConfig.meta.name}
+        </h1>
+        <div className="mt-2 text-2xl font-semibold italic md:text-2xl lg:text-3xl">
+          {siteConfig.meta.tagline}
         </div>
       </div>
     </section>
