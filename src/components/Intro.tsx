d8626a8a33f774385500257bd8658fa935e575aa
Dan Stroot <dan.stroot@gmail.com>
2023-09-02 08:17:59 -0700

    updated dependencies

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -6,7 +6,7 @@ export const Intro = () => {
       <div className="flex flex-col">
         <div className="flex flex-col items-center md:flex-row md:justify-between">
           <p className="text-4xl font-bold leading-tight tracking-tighter md:text-6xl lg:text-7xl">
-            {siteConfig.meta.name + ' · Blog'}
+            {siteConfig.meta.name}
           </p>
           <p className="mt-2 text-2xl font-semibold italic md:text-2xl lg:text-3xl">
             {siteConfig.meta.tagline}.
