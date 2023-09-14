e4f2f94a9c6ccff76cb5265d0ce096e0dd4c643b
Dan Stroot <dan.stroot@gmail.com>
2023-09-14 07:05:22 -0700

    updated dependencies

--- src/lib/getLocalBase64.ts
+++ src/lib/getLocalBase64.ts
@@ -9,10 +9,7 @@ export default async function getBase64(imageUrl: string) {
     }
 
     const buffer = await res.arrayBuffer()
-
     const { base64 } = await getPlaiceholder(Buffer.from(buffer))
-
-    //console.log(`base64: ${base64}`)
 
     return base64
   } catch (e) {
