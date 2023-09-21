bdd4e405b824329f86ffe785ce4f116ac35f2483
Dan Stroot <dan.stroot@gmail.com>
2023-09-20 20:41:15 -0700

    updated dependencies

--- src/components/MDXComponents/CustomImage.tsx
+++ src/components/MDXComponents/CustomImage.tsx
@@ -1,20 +1,16 @@
-import NextImage, { ImageProps } from 'next/image'
+import Image, { ImageProps } from 'next/image'
 
 export const CustomImage = (props: React.HTMLAttributes<HTMLImageElement>) => {
   let imgProps = props as unknown as ImageProps
   let src = imgProps.src as string
-  const isInternalImage = src && src.startsWith('/')
 
-  // https://github.com/contentlayerdev/contentlayer/issues/506
-  const Image = (props: ImageProps) => <NextImage {...props} />
+  // external images start with http*, website images start with /
+  const isInternalImage = src && src.startsWith('/')
 
   if (isInternalImage) {
     return (
-      // height and width are part of the props, from rehype-img-size
-      // so they are automatically passed here with {...props}
-
-      // https://www.peterlunch.com/snippets/next-image-styling
       <div className="relative aspect-[2/1]">
+        {/* eslint-disable-next-line jsx-a11y/alt-text */}
         <Image
           {...imgProps}
           fill
@@ -28,7 +24,7 @@ export const CustomImage = (props: React.HTMLAttributes<HTMLImageElement>) => {
 
   return (
     <div className="relative aspect-[2/1]">
-      {/* eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text */}{' '}
+      {/* eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text */}
       <img
         className="max-h-[384px]"
         src={imgProps.src + '?w=768'}
