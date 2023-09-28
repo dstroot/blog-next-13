54dbd2c1538bf4804ded5ee10b8ee1d6a3b35897
Dan Stroot <dan.stroot@gmail.com>
2023-09-27 18:37:27 -0700

    updated dependencies

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import { Container } from '@/components/Container'
 export default function Loading() {
   return (
     <Container variant="padded">
-      <p className="text-xl font-medium text-muted">Loading...</p>
+      <p className="text-xl font-medium">Loading...</p>
     </Container>
   )
 }
--- src/app/posts/[slug]/loading.tsx
+++ src/app/posts/[slug]/loading.tsx
@@ -6,7 +6,7 @@ import { Container } from '@/components/Container'
 export default function Loading() {
   return (
     <Container variant="padded">
-      <p className="text-xl font-medium text-muted">Loading...</p>
+      <p className="text-xl font-medium">Loading...</p>
     </Container>
   )
 }
