0664bc5c787a6bee6597267339488a68912e5a31
Dan Stroot <dan.stroot@gmail.com>
2024-01-05 09:43:17 -0800

    updated quotes

--- src/lib/getRandomQuote.ts
+++ src/lib/getRandomQuote.ts
@@ -8,23 +8,28 @@ export type Quote = {
   comments?: string
 }
 
-const prevQuoteObj = {
-  prev: 0,
-  setPrev: function (num: number) {
-    this.prev = num
-  },
+let myArray: number[] = []
+
+function saveIndex(addIndex: number) {
+  // if array is more than five items, remove first one
+  if (myArray.length === 5 && myArray.length > 1) {
+    myArray.shift()
+  }
+
+  myArray.push(addIndex)
 }
 
 export function getRandomQuote(): Quote {
-  let randomIndex = prevQuoteObj.prev
+  // start with a random number
+  let randomIndex = Math.floor(Math.random() * quotes.length)
 
-  // iterate until the quote id is not the same as the previous quote id
-  while (randomIndex === prevQuoteObj.prev) {
+  // iterate until the quote id is *not* the same as any in array
+  while (myArray.includes(randomIndex)) {
     randomIndex = Math.floor(Math.random() * quotes.length)
   }
 
-  // save new quote id
-  prevQuoteObj.setPrev(randomIndex)
+  // save new quote id in array
+  saveIndex(randomIndex)
 
-  return quotes[randomIndex] // return object (it's not returning an arr)
+  return quotes[randomIndex] // return object (it's not returning an array)
 }
