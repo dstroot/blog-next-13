a5b7471b51109a8e13bcc6c27d282e87dc663f39
Dan Stroot <dan.stroot@gmail.com>
2024-04-06 09:16:40 -0700

    updated dependencies

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -9,34 +9,16 @@ import { Container } from '@/components/Container'
 import { PageHeader } from '@/components/PageHeader'
 import { GitHubLink } from '@/components/posts/GitHubLink'
 
-interface PageProps {
-  params: {
-    slug: string[]
-  }
-}
-
-function getPageFromParams(params: PageProps['params']) {
-  //   console.log('Params: ' + params.slug)
-  const slug = params?.slug?.join('/') ?? ''
-  //   console.log('Slug: ' + slug)
-  const page = pages.find((page: { slug: string }) => page.slug === slug)
-  //   console.log("Page: " + page)
-
-  if (!page) {
-    return null
-  }
-
-  return page
-}
-
 /*
 NOTE: OG image is created by the og api route, there is no real
       image associated with non-blog posts. So we generate one.
 */
 export async function generateMetadata({
   params,
-}: PageProps): Promise<Metadata> {
-  const page = getPageFromParams(params)
+}: {
+  params: { slug: string }
+}): Promise<Metadata> {
+  const page = pages.find((page: { slug: string }) => page.slug === params.slug)
 
   if (!page) {
     return {}
@@ -75,27 +57,16 @@ export async function generateMetadata({
   }
 }
 
-export async function generateStaticParams(): Promise<PageProps['params'][]> {
-  return pages.map((page) => ({
-    slug: page.slug.split('/'),
-  }))
+export const generateStaticParams = async () => {
+  return pages.map((page) => ({ slug: page.slug }))
 }
 
-export default function Page({ params }: PageProps) {
+export default function Page({ params }: { params: { slug: string } }) {
   // Find the page for the current slug.
-  const page = getPageFromParams(params)
-  //   const page = pages.find(
-  //     (page: { slug: string }) => page.slug === params.slug[0],
-  //   )
+  const page = pages.find((page: { slug: string }) => page.slug === params.slug)
 
   // 404 if the post does not exist.
   if (!page) notFound()
-
-  //   // Remove the /pages prefix from the slug
-  //   const formattedPage = {
-  //     ...page,
-  //     slug: page.slug.replace(/^\/pages/, ''),
-  //   }
 
   const github = `${env.NEXT_PUBLIC_GITHUB_REPO}/blob/master/content/pages${page.permalink}.mdx`
 
