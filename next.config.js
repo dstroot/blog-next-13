f9f10669657968d42cecb2ea50e1c58c3225be33
Dan Stroot <dan.stroot@gmail.com>
2024-04-06 13:11:50 -0700

    working velite

--- next.config.js
+++ next.config.js
@@ -39,10 +39,6 @@ const nextConfig = {
       },
     ]
   },
-  //   webpack: (config) => {
-  //     config.plugins.push(new VeliteWebpackPlugin())
-  //     return config
-  //   },
 }
 
 module.exports = withPlugins(
@@ -52,18 +48,3 @@ module.exports = withPlugins(
   ],
   nextConfig,
 )
-
-class VeliteWebpackPlugin {
-  static started = false
-  apply(/** @type {import('webpack').Compiler} */ compiler) {
-    // executed three times in nextjs:
-    // twice for the server (nodejs / edge runtime) and once for the client
-    compiler.hooks.beforeCompile.tap('VeliteWebpackPlugin', async () => {
-      if (VeliteWebpackPlugin.started) return
-      VeliteWebpackPlugin.started = true
-      const dev = compiler.options.mode === 'development'
-      const { build } = await import('velite')
-      await build({ watch: dev, clean: !dev })
-    })
-  }
-}
