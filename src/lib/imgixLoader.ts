187307df7ba9271866788bad8bb8a4f2976f81ec
Dan Stroot <dan.stroot@gmail.com>
2024-04-18 15:00:17 -0700

    working blog

--- src/lib/imgixLoader.ts
+++ src/lib/imgixLoader.ts
@@ -16,12 +16,15 @@ export default function imgixLoader({ src, width, quality }: LoaderProps) {
   const url = new URL(`${imgixURL}/${normalizeSrc(src)}`)
   const params = url.searchParams
 
-  params.set('auto', params.getAll('auto').join(',') || 'format')
+  params.set('auto', params.getAll('auto').join(',') || 'format,enhance')
+  params.set('fm', params.getAll('fm').join(',') || 'avif,webp')
   params.set('fit', params.get('fit') || 'max')
   params.set('w', params.get('w') || width.toString())
 
   if (quality) {
-    params.set('q', quality.toString() || '50')
+    params.set('q', params.get('w') || '75')
+  } else {
+    params.set('q', '75')
   }
 
   return url.href
