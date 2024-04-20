a42981d7e4c5ced14757d67d704cc7c9fc8ea9c0
Dan Stroot <dan.stroot@gmail.com>
2024-04-20 08:47:18 -0700

    working blog

--- src/components/MDXComponents/YouTube.tsx
+++ src/components/MDXComponents/YouTube.tsx
@@ -4,6 +4,7 @@ export const YouTube = ({ link }: { link: string }) => {
     <iframe
       title="YouTube iFrame Component"
       className="aspect-video w-full"
+      loading="lazy"
       src={'https://www.youtube-nocookie.com/embed/' + url.pathname}
       // frameBorder="0"
       allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
