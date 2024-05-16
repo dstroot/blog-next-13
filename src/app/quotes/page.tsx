5dc5fb22e3b5554f29faeeae3853a7dc79969be0
Dan Stroot <dan.stroot@gmail.com>
2024-05-16 16:53:22 -0700

    updated dependencies

--- src/app/quotes/page.tsx
+++ src/app/quotes/page.tsx
@@ -1,7 +1,10 @@
-import { getRandomQuote } from '@/lib/getRandomQuote'
+// import { getRandomQuote } from '@/lib/getRandomQuote'
+
+// import { Quote } from '@/components/Quote'
+import { quotes } from 'velite/generated'
+
 import { Container } from '@/components/Container'
 import { PageHeader } from '@/components/PageHeader'
-import { Quote } from '@/components/Quote'
 
 export const dynamic = 'force-dynamic'
 
@@ -10,7 +13,7 @@ export const generateMetadata = () => {
 }
 
 export default function QuotesPage() {
-  const randomQuote = getRandomQuote()
+  //   const randomQuote = getRandomQuote()
 
   return (
     <Container variant="padded">
@@ -18,7 +21,24 @@ export default function QuotesPage() {
         title="Quotes"
         description="Inspiring words. Beautifully expressed."
       />
-      <Quote {...randomQuote} />
+      {/* <Quote {...randomQuote} /> */}
+
+      {/* Grid */}
+      <div className="mb-6 grid w-full grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-3">
+        {quotes.map((quote, _idx) => (
+          //   <p className="text-balance border-gray-500 text-lg font-medium italic text-gray-900 dark:text-white md:text-lg">
+          //     {quote.quote}
+          //   </p>
+
+          <a href={`/quotes/${quote.id}`}>
+            <div className="border border-gray-500">
+              <p className="text-balance p-6 text-lg font-medium italic text-gray-900 dark:text-white md:text-lg">
+                {quote.quote}
+              </p>
+            </div>
+          </a>
+        ))}
+      </div>
     </Container>
   )
 }
