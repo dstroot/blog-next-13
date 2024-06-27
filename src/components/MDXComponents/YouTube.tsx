07bac98ac17feb3c6729fb041a3852fec4e08a91
Dan Stroot <dan.stroot@gmail.com>
2024-06-27 09:51:19 -0700

    added YouTube shorts support

--- src/components/MDXComponents/YouTube.tsx
+++ src/components/MDXComponents/YouTube.tsx
@@ -1,5 +1,20 @@
-export const YouTube = ({ link }: { link: string }) => {
+export const YouTube = ({ link, short }: { link: string; short: boolean }) => {
   const url = new URL(link)
+
+  if (short) {
+    return (
+      <iframe
+        title="YouTube iFrame Component"
+        className="aspect-[9/16] h-[750px]"
+        loading="lazy"
+        src={'https://www.youtube-nocookie.com/embed/' + url.pathname}
+        // frameBorder="0"
+        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    )
+  }
+
   return (
     <iframe
       title="YouTube iFrame Component"
