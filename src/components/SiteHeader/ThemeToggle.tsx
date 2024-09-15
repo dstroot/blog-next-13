dc61e02183618e7b1b64f6f5101af14b7952a9a4
Dan Stroot <dan.stroot@gmail.com>
2024-09-15 12:12:35 -0700

    updated dependenciesq

--- src/components/SiteHeader/ThemeToggle.tsx
+++ src/components/SiteHeader/ThemeToggle.tsx
@@ -51,21 +51,51 @@ export function ThemeToggle() {
               className={cn(theme === 'light' && 'bg-accent')}
               onClick={() => setTheme('light')}
             >
-              <Icons.sun className="mr-2 h-4 w-4" />
+              {/* <Icons.sun className="mr-2 h-4 w-4" /> */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 256 256"
+                className="mr-2 h-4 w-4"
+              >
+                <path d="M120,40V16a8,8,0,0,1,16,0V40a8,8,0,0,1-16,0Zm72,88a64,64,0,1,1-64-64A64.07,64.07,0,0,1,192,128Zm-16,0a48,48,0,1,0-48,48A48.05,48.05,0,0,0,176,128ZM58.34,69.66A8,8,0,0,0,69.66,58.34l-16-16A8,8,0,0,0,42.34,53.66Zm0,116.68-16,16a8,8,0,0,0,11.32,11.32l16-16a8,8,0,0,0-11.32-11.32ZM192,72a8,8,0,0,0,5.66-2.34l16-16a8,8,0,0,0-11.32-11.32l-16,16A8,8,0,0,0,192,72Zm5.66,114.34a8,8,0,0,0-11.32,11.32l16,16a8,8,0,0,0,11.32-11.32ZM48,128a8,8,0,0,0-8-8H16a8,8,0,0,0,0,16H40A8,8,0,0,0,48,128Zm80,80a8,8,0,0,0-8,8v24a8,8,0,0,0,16,0V216A8,8,0,0,0,128,208Zm112-88H216a8,8,0,0,0,0,16h24a8,8,0,0,0,0-16Z"></path>
+              </svg>
               <span>Light</span>
             </DropdownMenuItem>
             <DropdownMenuItem
               className={cn(theme === 'dark' && 'bg-accent')}
               onClick={() => setTheme('dark')}
             >
-              <Icons.moon className="mr-2 h-4 w-4" />
+              {/* <Icons.moon className="mr-2 h-4 w-4" /> */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 256 256"
+                className="mr-2 h-4 w-4"
+              >
+                <path d="M233.54,142.23a8,8,0,0,0-8-2,88.08,88.08,0,0,1-109.8-109.8,8,8,0,0,0-10-10,104.84,104.84,0,0,0-52.91,37A104,104,0,0,0,136,224a103.09,103.09,0,0,0,62.52-20.88,104.84,104.84,0,0,0,37-52.91A8,8,0,0,0,233.54,142.23ZM188.9,190.34A88,88,0,0,1,65.66,67.11a89,89,0,0,1,31.4-26A106,106,0,0,0,96,56,104.11,104.11,0,0,0,200,160a106,106,0,0,0,14.92-1.06A89,89,0,0,1,188.9,190.34Z"></path>
+              </svg>
               <span>Dark</span>
             </DropdownMenuItem>
             <DropdownMenuItem
               className={cn(theme === 'system' && 'bg-accent')}
               onClick={() => setTheme('system')}
             >
-              <Icons.laptop className="mr-2 h-4 w-4" />
+              {/* <Icons.laptop className="mr-2 h-4 w-4" /> */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 256 256"
+                className="mr-2 h-4 w-4"
+              >
+                <path d="M224,72H208V64a24,24,0,0,0-24-24H40A24,24,0,0,0,16,64v96a24,24,0,0,0,24,24H152v8a24,24,0,0,0,24,24h48a24,24,0,0,0,24-24V96A24,24,0,0,0,224,72ZM40,168a8,8,0,0,1-8-8V64a8,8,0,0,1,8-8H184a8,8,0,0,1,8,8v8H176a24,24,0,0,0-24,24v72Zm192,24a8,8,0,0,1-8,8H176a8,8,0,0,1-8-8V96a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8Zm-96,16a8,8,0,0,1-8,8H88a8,8,0,0,1,0-16h40A8,8,0,0,1,136,208Zm80-96a8,8,0,0,1-8,8H192a8,8,0,0,1,0-16h16A8,8,0,0,1,216,112Z"></path>
+              </svg>
               <span>System</span>
             </DropdownMenuItem>
           </DropdownMenuContent>
